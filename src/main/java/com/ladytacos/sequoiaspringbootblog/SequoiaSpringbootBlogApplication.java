d456fda818668ebe82600e49161199c85f1ee73c
Mary Kathryn Syms <syms@ladytacos.com>
2018-01-23 14:16:32 -0600

    edited SequoiaSpringbootBlogApplication, pom.xl, created a .build_config file

--- src/main/java/com/ladytacos/sequoiaspringbootblog/SequoiaSpringbootBlogApplication.java
+++ src/main/java/com/ladytacos/sequoiaspringbootblog/SequoiaSpringbootBlogApplication.java
@@ -2,11 +2,18 @@ package com.ladytacos.sequoiaspringbootblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SequoiaSpringbootBlogApplication {
+public class SequoiaSpringbootBlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SequoiaSpringbootBlogApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SequoiaSpringbootBlogApplication.class);
 	}
 }
