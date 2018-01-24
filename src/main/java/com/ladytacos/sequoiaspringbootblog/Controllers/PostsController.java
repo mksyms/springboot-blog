cefc234046e11535f54c64af7b3ba79233a3ae3b
Mary Kathryn Syms <syms@ladytacos.com>
2018-01-24 12:09:45 -0600

    Commented out past steps and notes to keep track of workflow for reference

--- src/main/java/com/ladytacos/sequoiaspringbootblog/Controllers/PostsController.java
+++ src/main/java/com/ladytacos/sequoiaspringbootblog/Controllers/PostsController.java
@@ -4,25 +4,33 @@ import com.ladytacos.sequoiaspringbootblog.Models.Post;
 import com.ladytacos.sequoiaspringbootblog.Services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+//    Dependency injection
+// 1. Constructor injection (preferred) -> required dependencies
+// 2. Setter injection -> optional dependencies
+
 @Controller
 public class PostsController {
+            // 1. Create an instance variable with your dependency
     private final PostService postService;
 
+            // 2. Inject the dependency through the constructor and assign it to your instance variable
     public PostsController(PostService postService) {
-        this.postService = postService;
+        this.postService = postService; // This the first time we assign something to service
     }
 
     @RequestMapping("/posts")
     public String viewPosts(Model vModel) {
-        List<Post> posts= postService.findAllPosts();
+        List<Post> posts = postService.findAllPosts();
+//        List<Post> posts = Arrays.asList(
+//        new Post("Post A", "Body A");
+//        new Post("Post B", "Body B");
+//        new Post("Post C", "Body C");
+
 
         vModel.addAttribute("posts", posts);
 
@@ -31,19 +39,20 @@ public class PostsController {
 
     @RequestMapping("/posts/{id}")
     public String viewIndividualPost(@PathVariable String id, Model vModel){
+//        Post post = new Post("Test post", "Test body");
         Post post = new Post(1, "First Post", "Bringing it back to emotionally angsty LiveJournal Days!");
         vModel.addAttribute("post", post);
         vModel.addAttribute("id", id);
         return "posts/show";
     }
 
-    @GetMapping("/posts/view")
+    @RequestMapping("/posts/view")
     @ResponseBody
     public String viewCreateForm() {
         return "View form to create a post";
     }
 
-    @GetMapping("/posts/create")
+    @PostMapping("/posts/create")
     @ResponseBody
     public String createPost() {
         return "Create a new post";
